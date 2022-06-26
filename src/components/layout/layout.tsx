295ba578756ecdb43f14676993821b929b34fc81
deebeel22 <deedeeua@gmail.com>
2022-06-26 12:52:01 +0300

    add mobile menu

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -4,7 +4,7 @@ import {Nav} from './navigation';
 
 export function Layout({children}: GenericProps<{ }>) {
     return <div
-        className="relative grid mt-2 mx-2 grid-cols-[215px_1fr] gap-1
+        className="relative grid mt-2 mx-2 grid-cols-[1fr] gap-1
         md:mx-24 md:grid-cols-[215px_minmax(700px,_1fr)] md:gap-2">
         <div className="flex flex-col m-1 gap-4">
             <ArtistName/>
--- src/components/layout/navigation/nav.tsx
+++ src/components/layout/navigation/nav.tsx
@@ -1,24 +1,55 @@
 import {NavGroup} from './navGroup';
 import {NavItem} from './navItem';
 import {graphql, useStaticQuery} from 'gatsby';
+import HamburgerIcon from 'components/icons/hamburger.svg'
+import {useState} from 'react';
+import classNames from 'classnames';
+
+
+type WorkNavigation = {
+    path: string;
+    pageContext: { title: string };
+}
 
 export function Nav() {
     const workNav = useWorkNavigationInfo();
-    return <nav className="flex flex-col gap-3">
+    return <nav>
+        <MobileNav works={workNav}/>
+        <DesktopNav works={workNav}/>
+    </nav>;
+}
+
+function MobileNav({works}: { works: WorkNavigation[] }) {
+    const [isOpen, setOpen] = useState(false);
+
+    return <section className="md:hidden" onClick={() => isOpen && setOpen(false)}>
+        <HamburgerIcon className="absolute top-0 right-0 w-8 h-8" onClick={() => setOpen(v => !v)}/>
+        <div className={classNames('flex-col gap-3 absolute z-10 right-0 w-full h-screen bg-gray-300/95', {
+            hidden: !isOpen,
+            flex: isOpen
+        })}>
+            <NavInternal workNavigations={works}/>
+        </div>
+    </section>
+}
+
+function DesktopNav({works}: { works: WorkNavigation[] }) {
+    return <div className="flex-col gap-3 hidden md:flex">
+        <NavInternal workNavigations={works}/>
+    </div>
+}
+
+function NavInternal({workNavigations}: { workNavigations: WorkNavigation[] }) {
+    return <>
         <NavGroup>
-            {workNav.map(n => <NavItem key={n.path} location={n.path}>{n.pageContext.title}</NavItem>)}
+            {workNavigations.map(n => <NavItem key={n.path} location={n.path}>{n.pageContext.title}</NavItem>)}
         </NavGroup>
         <NavGroup>
             <NavItem location="/about">About</NavItem>
             <NavItem location="/contacts">Contacts</NavItem>
             <NavItem location="/">Events</NavItem>
         </NavGroup>
-    </nav>;
-}
-
-type WorkNavigation = {
-    path: string;
-    pageContext: { title: string };
+    </>;
 }
 
 function useWorkNavigationInfo() {
